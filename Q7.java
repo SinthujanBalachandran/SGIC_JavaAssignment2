b7190582b928d13df5aaf098b8e5c310196a45ec
SinthujanBalachandran <sinthujan2233@gmail.com>
2019-09-09 23:56:33 +0530

    ass 2

---
+++ Q7.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Enter word");
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        
+                char[] inputArray = word.toCharArray();
+                int countVow = 0;
+                int countCon = 0;
+                //if (word.matches(word)){}
+                for(char i :inputArray){
+                    
+                    System.out.println(i);
+                    
+                
+                }
+
+    }
+    
+}
